112c40338f6c2655163183b8ca0f8a99d2716f92
Mike Munhall <munhall.mike@gmail.com>
2016-10-26 10:59:20 -0600

    Changed if statements to matchers

--- src/main/scala/com/mikemunhall/katas/scala/Factorial.scala
+++ src/main/scala/com/mikemunhall/katas/scala/Factorial.scala
@@ -5,8 +5,11 @@ object Factorial {
   // Factorial e.g.: 5! == 5 * 4 * 3 * 2 * 1
   //           e.g.: 0! == 1 (a special case)
   def getResult(x: Int): Int = {
-    if (x < 0) throw new IllegalArgumentException
-    if (x == 0) 1 else x * getResult(x - 1)
+    x match {
+      case x if x < 0 => throw new IllegalArgumentException
+      case x if x == 0 => 1
+      case x => getResult(x - 1) * x
+    }
   }
 
 }
